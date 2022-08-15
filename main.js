381e75b48b0b8a22e776a5de6d29f23141f2bfbe
Anne Hölscher <anne.k.hoelscher@gmail.com>
2022-08-15 11:51:44 +0100

    first try dynamic js

--- main.js
+++ main.js
@@ -9,3 +9,34 @@ console.log(wordCards);
 wordCards.forEach((wordCard) => {
   Card(wordCard);
 });
+
+const main = document.querySelector('[data-js="main"]');
+
+const riddles = [
+  { question: "Was ist der Unterschied zwischen einer Ente?", answer: "tja" },
+  { question: "Wie warm ist es über den Berg?", answer: "2Grad" },
+  { question: "Ja?", answer: "nein" },
+];
+
+riddles.forEach((riddle) => {
+  const questionCard = document.createElement("article");
+  questionCard.classList.add("card");
+
+  const question = document.createElement("h2");
+  question.innerText = riddle.question;
+
+  const button = document.createElement("button");
+  button.classList.add("card__button");
+  button.innerText = "show answer";
+  button.setAttribute("data-js", "button");
+
+  const answer = document.createElement("p");
+  answer.classList.add("card__answer", "hidden");
+  answer.innerText = riddle.answer;
+  answer.setAttribute("data-js", "answer");
+
+  questionCard.append(question, button, answer);
+  main.append(questionCard);
+
+  Card(questionCard);
+});
