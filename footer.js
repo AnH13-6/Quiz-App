2ddf9c471eefd3453095c90bfa53d69dd3090897
Anne Hölscher <anne.k.hoelscher@gmail.com>
2022-08-17 19:34:22 +0100

    added function to form

--- footer.js
+++ footer.js
@@ -3,6 +3,7 @@ export default function Navigation() {
   const buttonBookmark = document.querySelector('[data-nav="bookmark"]');
   const buttonCreate = document.querySelector('[data-nav="create"]');
   const buttonProfile = document.querySelector('[data-nav="profile"]');
+  const headline = document.querySelector("[data-js=headline]");
 
   const pageHome = document.querySelector('[data-page="home"]');
   const pageBookmark = document.querySelector('[data-page="bookmark"]');
@@ -14,6 +15,7 @@ export default function Navigation() {
     pageBookmark.classList.add("hidden");
     pageCreate.classList.add("hidden");
     pageProfile.classList.add("hidden");
+    headline.textContent = "Kanji 漢字";
   });
 
   buttonBookmark.addEventListener("click", () => {
@@ -21,6 +23,7 @@ export default function Navigation() {
     pageHome.classList.add("hidden");
     pageCreate.classList.add("hidden");
     pageProfile.classList.add("hidden");
+    headline.innerText = "Bookmarks";
   });
 
   buttonCreate.addEventListener("click", () => {
@@ -28,6 +31,7 @@ export default function Navigation() {
     pageBookmark.classList.add("hidden");
     pageHome.classList.add("hidden");
     pageProfile.classList.add("hidden");
+    headline.textContent = "Create";
   });
 
   buttonProfile.addEventListener("click", () => {
@@ -35,5 +39,6 @@ export default function Navigation() {
     pageCreate.classList.add("hidden");
     pageHome.classList.add("hidden");
     pageBookmark.classList.add("hidden");
+    headline.textContent = "Profile";
   });
 }
